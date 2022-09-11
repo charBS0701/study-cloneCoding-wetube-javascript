bbb6f33e493ee4eae7ab2c721fe3fe966ed514c8
charbs0701 <chaqhdtjr7@gmail.com>
2022-09-11 20:10:32 +0900

    views 폴더 생성

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -1,6 +1,6 @@
 /** 홈화면에서 추천 영상을 띄워주는 함수 */
-export const trending = (req, res) => res.send("Home page Videos");
-export const  see = (req, res) => {
+export const trending = (req, res) => res.render("home.pug");
+export const see = (req, res) => {
   console.log(req.params);
   return res.send(`Watch Video ${req.params.id}`);
 };
--- src/server.js
+++ src/server.js
@@ -1,19 +1,33 @@
 import express from "express";
-import mogan from "morgan";
+import morgan from "morgan";
 import globalRouter from "./routers/globalRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
-
+var uuid = require("short-uuid");
 const PORT = 4000;
 
 const app = express();
-const logger = mogan("dev");
+const logger = morgan("common");
 
+morgan.token("id", function getId(req) {
+  return req.id;
+});
+
+app.set("view engine", "pug");
+
+app.use(assignId);
+app.use(morgan(":id :method :url :response-time"));
+
+function assignId(req, res, next) {
+  req.id = uuid();
+  next();
+}
+
+// setup the logger
+// app.use(logger);
 app.use("/", globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
-
-app.use(logger);
 
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT} 👻`);
