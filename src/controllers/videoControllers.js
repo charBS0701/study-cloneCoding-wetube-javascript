2c5a9d31d2f0930939b63deb657fd1ed3cd97701
charBS0701 <chaqhdtjr7@gmail.com>
2022-09-12 18:21:45 +0900

    #6.11 complete. connected to mongo

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -1,39 +1,30 @@
+import Video from "../models/Video";
+
 /** 홈화면에서 추천 영상을 띄워주는 함수 */
-export const trending = (req, res) => {
-  const videos = [
-    {
-      title: "First Video",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 minutes ago",
-      views: 59,
-      id: 1,
-    },
-    {
-      title: "Second Video",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 minutes ago",
-      views: 59,
-      id: 1,
-    },
-    {
-      title: "Third Video",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 minutes ago",
-      views: 59,
-      id: 1,
-    },
-  ];
-  return res.render("home", { pageTitle: "Home", videos });
+export const home = (req, res) => {
+  Video.find({}, (error, videos) => {});
+  return res.render("home", { pageTitle: "Home" });
 };
 
-export const see = (req, res) => res.render("watch");
-export const edit = (req, res) => res.render("edit");
-export const search = (req, res) => res.send("Search");
-export const upload = (req, res) => res.send("Upload");
-export const deleteVideo = (req, res) => {
-  console.log(req.params);
-  return res.send("Delete Video");
+export const watch = (req, res) => {
+  const { id } = req.params;
+  return res.render("watch", { pageTitle: `Watching` });
+};
+export const getEdit = (req, res) => {
+  const { id } = req.params;
+  return res.render("edit", { pageTitle: `Editing` });
+};
+export const postEdit = (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+  return res.redirect(`/videos/${id}`);
+};
+
+export const getUpload = (req, res) => {
+  return res.render("upload", { pageTitle: "Upload Video" });
+};
+
+export const postUpload = (req, res) => {
+  const { title } = req.body;
+  return res.redirect("/");
 };
--- src/server.js
+++ src/server.js
@@ -3,34 +3,17 @@ import morgan from "morgan";
 import globalRouter from "./routers/globalRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
-var uuid = require("short-uuid");
-const PORT = 4000;
 
 const app = express();
-const logger = morgan("common");
-
-morgan.token("id", function getId(req) {
-  return req.id;
-});
+const logger = morgan("dev");
 
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
-
-app.use(assignId);
-app.use(morgan(":id :method :url :response-time"));
-
-function assignId(req, res, next) {
-  req.id = uuid();
-  next();
-}
-
 // setup the logger
-// app.use(logger);
+app.use(logger);
+app.use(express.urlencoded({ extended: true }));
 app.use("/", globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
 
-const handleListening = () =>
-  console.log(`Server listening on port http://localhost:${PORT} 👻`);
-
-app.listen(PORT, handleListening);
+export default app;
