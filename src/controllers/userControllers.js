53d1e7429ede0a3edd3b563c5751c36684fbe65d
charbs0701 <chaqhdtjr7@gmail.com>
2023-01-26 20:17:46 +0900

    FEAT github oAuth

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -84,6 +84,7 @@ export const finishGithubLogin = async (req, res) => {
   };
   const params = new URLSearchParams(config).toString();
   const finalUrl = `${baseUrl}?${params}`;
+  // 토큰 획득
   const tokenRequest = await (
     await fetch(finalUrl, {
       method: "POST",
@@ -92,29 +93,60 @@ export const finishGithubLogin = async (req, res) => {
       },
     })
   ).json();
-  console.log("access_token: " + JSON.stringify(tokenRequest));
+
+  // 토큰 획득 시
   if ("access_token" in tokenRequest) {
     const { access_token } = tokenRequest;
-    const userRequest = await (
-      await fetch("https://api.github.com/user", {
+    const apiUrl = "https://api.github.com";
+    // 토큰을 이용해 깃허브 api요청
+    const userData = await (
+      await fetch(`${apiUrl}/user`, {
         headers: {
-          Authorization: `token ${access_token}`,
+          Authorization: `Bearer ${access_token}`,
         },
       })
     ).json();
-
-    console.log("user information : " + JSON.stringify(userRequest));
+    const emailData = await (
+      await fetch(`${apiUrl}/user/emails`, {
+        headers: {
+          Authorization: `Bearer ${access_token}`,
+        },
+      })
+    ).json();
+    // 받은 github에서 이메일찾기
+    const emailObj = emailData.find(
+      (email) => email.primary === true && email.verified === true
+    );
+    if (!emailObj) {
+      return res.redirect("/login");
+    }
+    // db에 일치하는 email 찾기
+    let user = await User.findOne({ email: emailObj.email });
+    // db에 일지하는 email 없으면 socialOnly로 추가
+    if (!user) {
+      user = await User.create({
+        avatarUrl: userData.avatarUrl,
+        name: userData.name,
+        username: userData.login,
+        email: emailObj.email,
+        password: "",
+        socialOnly: true,
+        location: userData.location,
+      });
+    }
+    // 세션 추가
+    req.session.loggedIn = true;
+    req.session.user = user;
     return res.redirect("/");
   } else {
-    return res.redirect("/join");
+    return res.redirect("/login");
   }
 };
-export const logout = (req, res) => res.send("Log out");
 
-export const getEdit = (req, res) => {
-  return res.render("edit-profile", { pageTitle: "Edit Profile" });
+export const logout = (req, res) => {
+  req.session.destroy();
+  return res.redirect("/");
 };
-export const postEdit = (req, res) => {
-  return res.render("edit-profile");
-};
-export const see = (req, res) => res.send("See");
+
+export const edit = (req, res) => res.send("Edit User");
+export const see = (req, res) => res.send("See User");
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,25 +1,16 @@
 import express from "express";
 import {
   edit,
-  remove,
   logout,
   see,
   startGithubLogin,
   finishGithubLogin,
-  getEdit,
-  postEdit,
 } from "../controllers/userControllers";
-
-import { uploadFiles } from "../middlewares";
 
 const userRouter = express.Router();
 
 userRouter.get("/logout", logout);
-userRouter
-  .route("/edit")
-  .get(getEdit)
-  .post(uploadFiles.single("avatar"), postEdit);
-userRouter.get("/remove", remove);
+userRouter.get("/edit", edit);
 userRouter.get("/github/start", startGithubLogin);
 userRouter.get("/github/finish", finishGithubLogin);
 userRouter.get(":id", see);
