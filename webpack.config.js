b97b491f68a4a8ecb7d3ec4ed06e52151c2617f5
charbs0701 <chaqhdtjr7@gmail.com>
2023-01-31 15:51:22 +0900

    FEAT SCSS Loader, CHORE Config files Better Developer Experience, nodemon.json

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,19 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require("path");
 
 module.exports = {
   entry: "./src/client/js/main.js",
   mode: "development",
+  watch: true, // 변경되면 재시작
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "css/styles.css",
+    }),
+  ],
   output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "assets", "js"),
+    filename: "xxxx/main.js",
+    path: path.resolve(__dirname, "assets"),
+    clean: true, // clean output folder before build
   },
   module: {
     rules: [
@@ -18,6 +26,10 @@ module.exports = {
             presets: [["@babel/preset-env", { targets: "defaults" }]],
           },
         },
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"], // webpack은 뒤에서부터
       },
     ],
   },
