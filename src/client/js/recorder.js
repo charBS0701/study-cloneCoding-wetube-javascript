dec7d3dd453bc02ef48d0d4e9bde5057f102d620
charbs0701 <chaqhdtjr7@gmail.com>
2023-02-13 16:56:51 +0900

    DESIGN Upload CSS

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -69,17 +69,10 @@ const handleDownload = async () => {
   actionBtn.addEventListener("click", handleStart);
 };
 
-const handleStop = () => {
-  actionBtn.innerText = "Download Recording";
-  actionBtn.removeEventListener("click", handleStop);
-  actionBtn.addEventListener("click", handleDownload);
-  recorder.stop();
-};
-
 const handleStart = () => {
-  actionBtn.innerText = "Stop Recording";
+  actionBtn.innerText = "Recording";
+  actionBtn.disabled = true;
   actionBtn.removeEventListener("click", handleStart);
-  actionBtn.addEventListener("click", handleStop);
   recorder = new MediaRecorder(stream, { mimeType: "video/webm" });
   recorder.ondataavailable = (event) => {
     videoFile = URL.createObjectURL(event.data);
@@ -87,8 +80,14 @@ const handleStart = () => {
     video.src = videoFile;
     video.loop = true;
     video.play();
+    actionBtn.innerText = "Download";
+    actionBtn.disabled = false;
+    actionBtn.addEventListener("click", handleDownload);
   };
   recorder.start();
+  setTimeout(() => {
+    recorder.stop();
+  }, 5000);
 };
 
 const init = async () => {
@@ -96,7 +95,10 @@ const init = async () => {
   stream = await navigator.mediaDevices.getUserMedia({
     // 마이크, 카메라에 접근
     audio: false,
-    video: true,
+    video: {
+      width: 1024,
+      height: 576,
+    },
   });
   video.srcObject = stream;
   video.play();
--- src/models/Video.js
+++ src/models/Video.js
@@ -4,7 +4,7 @@ const videoSchema = new mongoose.Schema({
   title: { type: String, required: true, trim: true, maxLength: 80 },
   fileUrl: { type: String, required: true },
   thumbUrl: { type: String, required: true },
-  description: { type: String, required: true, trim: true, minLength: 20 },
+  description: { type: String, required: true, trim: true, minLength: 2 },
   createdAt: { type: Date, required: true, default: Date.now },
   hashtags: [{ type: String, trim: true }],
   meta: {
@@ -19,9 +19,10 @@ videoSchema.static("formatHashtags", function (hashtags) {
     .split(",")
     .map((word) => (word.startsWith("#") ? word : `#${word}`));
 });
-videoSchema.static("changePathFormula", (urlPath) => {  // \\를 /로 변경
+videoSchema.static("changePathFormula", (urlPath) => {
+  // \\를 /로 변경
   return urlPath.replace(/\\/g, "/");
-  });
+});
 
 const Video = mongoose.model("Video", videoSchema);
 export default Video;
